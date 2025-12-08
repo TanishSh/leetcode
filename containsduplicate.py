afaa26b93dc54e0eb8716bc3a51e07147d04a3a3
TanishSh <tanishsharma.krish@gmail.com>
2025-12-07 23:59:22 -0500

    add comments to 217

--- containsduplicate.py
+++ containsduplicate.py
@@ -1,3 +1,12 @@
+# Sort the array in non-decreasing order.
+# Iterate through the array starting from index 1.
+# Compare the current element with the previous element.
+# If both elements are equal, we have found a duplicate — return True.
+# If the loop finishes without detecting equal neighbors, return False.
+
+#Time complexity: O(nlogn) -> as sort takes O(nlogn) in general/worst case
+# loop later only O(n) which is < O(nlogn) -> so O(nlogn) dominates
+# O(nlogn + n) = O(nlogn)
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
         nums.sort()
@@ -7,3 +16,5 @@ class Solution:
 
         return False
         
+# brute force -> check all pairs, each item starting at index 0... n-1 to all others items -> 0(n^2)
+
