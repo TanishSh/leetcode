379775547e78466db73558588a340048ed21be7a
TanishSh <tanishsharma.krish@gmail.com>
2025-12-23 18:54:01 -0500

    2 pointer 2 sum

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -10,3 +10,30 @@ class Solution:
                 li.append(i_2)
 
                 return li
+            
+# sorting: 2 pointer solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        A = []
+        for i, num in enumerate(nums):
+            A.append([num, i])
+
+        A.sort()
+
+        # 2 pointers
+        i, j = 0, len(nums) - 1
+
+        while i < j:
+            a1 = A[i][0] 
+            a2 = A[j][0]
+            curr = A[i][0] + A[j][0]
+            if curr == target:
+                return (min(a1, a2), max(a1, a2))
+            
+            # update the 2 pointers
+            elif curr < target:
+                i += 1 # curr < target, so move left pointer to inc. currer
+            else:
+                j+= 1 # curr < target, so move right  pointer to inc. curr
+
+        return []
