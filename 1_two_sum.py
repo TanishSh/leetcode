6c1a32e26978df4de60e2711b2bd94eb85545ec7
TanishSh <tanishsharma.krish@gmail.com>
2025-12-28 22:46:30 -0500

    last solution for 2 sum

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -58,14 +58,35 @@ class Solution:
         indices = {} #{ num: index}
 
         for i, n in enumerate(nums): # enumerate: index, value
-            indices[n] = i 
+            indices[n] = i  # create array that store index of each num
 
         for i, n in enumerate(nums):
             diff = target - n
+            # diff is 2nd num, it exists in array and the index of the num is not the same as the 1st (i.e. they are unique)
             if diff in indices and indices[diff] != i:
                 return [indices[diff], i]
             
         return []
+
+# hash map (one pass)
+# Create a hash map to store the value and index of each element in the array.
+# Iterate through the array and compute the complement of the current element, which is target - nums[i].
+# Check if the complement exists in the hash map.
+# If it does, return the indices of the current element and its complement.
+# If no such pair is found, return an empty array.
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {} #val -> index
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            
+            prevMap[n] = i
+
+
+
 
 
 
