d6bce9e514608c40ba41737ec2492f16c5ded19f
TanishSh <tanishsharma.krish@gmail.com>
2026-04-12 13:51:24 -0400

    group anagrams

--- 49_group_anagram.py
+++ 49_group_anagram.py
@@ -1,33 +1,13 @@
-# hash table
+from collections import defaultdict
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        li = [] # hashmap
+        groups = defaultdict(list)
+
         for s in strs:
             count = [0] * 26
-            for index, char in enumerate(s):
+            for char in s:
                 count[ord(char) - ord('a')] += 1
-            li.append(count)
+            groups[tuple(count)].append(s)
 
-        output = []
-        output.append([strs[0]])
-
-        unique_elements_index = [[0, len(output)-1]]
-        
-        for i in range(1, len(strs)):
-            found = False
-            for index, (u_i_li, u_i_output) in enumerate(unique_elements_index): # unique index
-                item_exists = any(strs[i] in sublist for sublist in output)
-                if i != u_i_li and not item_exists and li[i] == li[u_i_li]: 
-                    output[u_i_output].append(strs[i])
-                    Found = True
-                    break
-            
-            item_exists = any(strs[i] in sublist for sublist in output)
-            if not found and not item_exists:   
-                output.append([strs[i]])
-                unique_elements_index.append([i, len(output)-1])
-
-        return output
-
-        
-
+        return list(groups.values())
---
+++ 49_group_anagram_custom copy.py
@@ -0,0 +1,41 @@
+# hash table
+# issues with code:
+# inefficient: item_exists = any(strs[i] in sublist for sublist in output)
+# It searches through all groups every time just to see if the word is already there.
+# But this is unnecessary because each word is processed only once in order. You do not need to keep checking whether it already exists.
+
+# instead of manually comparing against old representatives, you can use a hashmap directly.
+
+# custom long solution
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        li = [] # hashmap
+        for s in strs:
+            count = [0] * 26
+            for index, char in enumerate(s):
+                count[ord(char) - ord('a')] += 1
+            li.append(count)
+
+        output = []
+        output.append([strs[0]])
+
+        unique_elements_index = [[0, len(output)-1]]
+        
+        for i in range(1, len(strs)):
+            found = False
+            for index, (u_i_li, u_i_output) in enumerate(unique_elements_index): # unique index
+                item_exists = any(strs[i] in sublist for sublist in output)
+                if i != u_i_li and not item_exists and li[i] == li[u_i_li]: 
+                    output[u_i_output].append(strs[i])
+                    Found = True
+                    break
+            
+            item_exists = any(strs[i] in sublist for sublist in output)
+            if not found and not item_exists:   
+                output.append([strs[i]])
+                unique_elements_index.append([i, len(output)-1])
+
+        return output
+
+        
+
